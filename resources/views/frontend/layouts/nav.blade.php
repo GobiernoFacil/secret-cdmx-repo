3d4e9e41d101ee51a216b605ca3e21b4e49ac776
hugovom <hugo.om@gmail.com>
2016-03-23 17:31:45 -0600

    add layout and home view

---
+++ resources/views/frontend/layouts/nav.blade.php
@@ -0,0 +1,14 @@
+<!--menu-->
+	<nav id="menu">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-3 col-sm-3">
+				<h2><a href="http://www.df.gob.mx/"  class="cdmx">CDMX | SEFIN</a></h2>
+				</div>
+				<div class="col-xs-6 col-sm-6">
+					<h1><a href="index.php">CONTRATACIONES <strong>ABIERTAS</strong></a></h1>
+				</div>
+				
+			</div>
+		</div>
+	</nav>
