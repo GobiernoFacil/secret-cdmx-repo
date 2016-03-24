37a50f297c932b2f26847220d455738aad5271a7
hugovom <hugo.om@gmail.com>
2016-03-23 19:01:49 -0600

    fix error in meta description

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -8,7 +8,7 @@
 	    <meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>{{ !empty($title) ?   $title  : "" }}</title>
-		<meta name="description" content="{{ !empty($description) ? "" :  $description }}">
+		<meta name="description" content="{{ !empty($description) ? $description : "" }}">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" type="text/css"  href="/css/normalize.css">
 		<link rel="stylesheet" type="text/css" href="/css/styles.css"/>
