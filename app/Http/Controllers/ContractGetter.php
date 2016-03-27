8e4f0bd3c0523767f4a75e3bf44472c03e1dae11
elcoruco <el.coruco@gmail.com>
2016-03-27 17:04:09 -0600

    more stuff

--- app/Http/Controllers/ContractGetter.php
+++ app/Http/Controllers/ContractGetter.php
@@ -8,7 +8,10 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use App\Models\Contract;
+use App\Models\Planning;
 use App\Models\Provider;
+use App\Models\Publisher;
+use App\Models\Release;
 
 class ContractGetter extends Controller
 {
@@ -42,12 +45,79 @@ class ContractGetter extends Controller
         'cvedependencia' => $c->cveDependencia, 
         'nomDependencia' => $c->nomDependencia
       ]);
+
+      // save extra data 
+      $meta = $this->getMetaDataX($contract);
+      if(!empty($meta) && ! property_exists($meta, 'error')){
+        // add extra data to contracts
+        $contract->uri = $meta->uri;
+        $contract->published_date = date("Y-m-d", strtotime($meta->publishedDate));
+        // create the publisher
+        $contract->publisher_id = Publisher::firstOrCreate([
+          "scheme" => $meta->publisher->scheme,
+          "name"   => $meta->publisher->name,
+          "uri"    => $meta->publisher->uri,
+          "uid"    => $meta->publisher->uid
+        ]);
+        // update contract
+        $contract->update();
+
+        // create releases
+        foreach($meta->releases as $r){
+          $release = Release::firstOrCreate([
+            "local_id"        => $r->id,
+            "contract_id"     => $contract->id,
+            "ocid"            => $contract->ocdsid,
+            "date"            => date("Y-m-d", strtotime($r->date)),
+            "initiation_type" => $r->initiationType,
+            "language"        => $r->language
+          ]);
+
+          // create planning
+          if($release->planning){
+            $planning = Planning::firstOrCreate([
+              "release_id" => $release->id
+            ]);
+
+            $planning->amount   = $r->planning->budget->amount->amount;
+            $planning->currency = $r->planning->budget->amount->currency;
+            $planning->project  = $r->planning->budget->project;
+
+            $planning->update();
+          }
+          /*
+| local_id        | int(11)          | NO   |     | NULL    |                |
+| contract_id     | int(11)          | NO   |     | NULL    |                |
+| ocid            | varchar(255)     | NO   |     | NULL    |                |
+| date            | date             | YES  |     | NULL    |                |
+| initiation_type | varchar(255)     | YES  |     | NULL    |                |
+| planning_id     | int(11)          | YES  |     | NULL    |                |
+| buyer_id        | int(11)          | YES  |     | NULL    |                |
+| tender_id       | int(11)          | YES  |     | NULL    |                |
+| language        | varchar(255)     | YES  |     | NULL    |                |
+          */
+        }
+      }
     }
     
-    var_dump($d);
+    echo ":D";
   }
 
-  private function 
+  private function getMetaDataX($contract){
+    // [1] make the call to the API
+    $url  = $this->apiContrato;
+    $data = ['dependencia' => $contract->cvedependencia, 'contrato' => $contract->ocdsid];
+
+    // [1.1] the CURL stuff
+    $ch   = curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS, http_build_query($data));
+    $result = curl_exec($ch);
+    $r      = json_decode($result);
+    return $r;
+  }
 
   public function getMetaData($ocid){
     // [1] Validate ocid & redirect if not valid
--- app/Models/Planning.php
+++ app/Models/Planning.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Planning extends Model
 {
+  protected $fillable = ["release_id", "amount", "currency", "project"];
   public function release(){
     return $this->belongsTo('App\Models\Release');
   }
