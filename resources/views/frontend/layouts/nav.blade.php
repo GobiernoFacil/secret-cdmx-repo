74fa4f623f573409d8d123cbef5b5ab48ecf6807
hugovom <hugo.om@gmail.com>
2016-04-01 19:35:34 -0600

    hide logo in home

--- resources/views/frontend/layouts/nav.blade.php
+++ resources/views/frontend/layouts/nav.blade.php
@@ -6,10 +6,13 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-xs-3 col-sm-3">
-					<h2><a href="http://www.df.gob.mx/"  class="cdmx">CDMX | SEFIN</a></h2>
+						<h2><a href="http://www.df.gob.mx/"  class="cdmx">CDMX</a></h2>
 					</div>
 					<div class="col-xs-6 col-sm-6">
+						@if ($body_class == 'home2') 
+						@else
 						<h1><a href="{{url('')}}">CONTRATACIONES <strong>ABIERTAS</strong></a></h1>
+						@endif
 					</div>
 					
 				</div>
