94d3af410e8c0b6946cc82a9bece11814e535601
hugovom <hugo.om@gmail.com>
2016-04-01 19:26:15 -0600

    add offices controller

---
+++ app/Http/Controllers/Offices.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Models\Contract;
+
+class Offices extends Controller
+{
+	
+	//
+	// Offices list
+	//	
+	public function index(){
+		$contracts 			 = Contract::all();
+		$data                = [];
+		$data['title']       = 'Dependencias de la CDMX con Contrataciones Abiertas';
+		$data['description'] = 'Dependencias de la CDMX con Contrataciones Abiertas';
+		$data['og_image']	 = "img/og/contrato-cdmx.png";
+		$data['body_class']  = 'dependencia';
+		
+		//// lista de contratos aún sin implementar en el view
+		$data['contracts']  = $contracts;
+		
+		return view("frontend.office")->with($data);
+	}
+
+	
+	
+	
+
+
+}
