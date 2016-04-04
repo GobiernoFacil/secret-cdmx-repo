305e90f542f7bbf9774e2159113411a83d3baccd
hugovom <hugo.om@gmail.com>
2016-04-04 17:38:34 -0500

    update contrato-landing app js

--- public/js/apps/contrato-landing/main.js
+++ public/js/apps/contrato-landing/main.js
@@ -7,7 +7,7 @@
 // @url     : http://gobiernofacil.com
 
 require.config({
-  baseUrl : "js/apps/contrato-landing",
+  baseUrl : "/js/apps/contrato-landing",
   paths : {
     d3          : "../../bower_components/d3/d3",
     jquery      : "../../bower_components/jquery/dist/jquery.min",
