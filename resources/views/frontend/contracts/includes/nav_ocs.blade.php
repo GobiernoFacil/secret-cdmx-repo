49a0247ac01192dabc5542d4dcb68ec9eb276fed
hugovom <hugo.om@gmail.com>
2016-04-04 18:19:00 -0500

    update nav

--- resources/views/frontend/contracts/includes/nav_ocs.blade.php
+++ resources/views/frontend/contracts/includes/nav_ocs.blade.php
@@ -4,12 +4,9 @@
 		<li><a href="#"  id="btn-contract-nav" class="nav_stage current" data-id="contracts" data-title="Contratación">
 			<?php echo file_get_contents("img/nav_contratacion.svg"); ?></a>
 			<ul id="nav_contract">
-			  <?php    $count_nav = 0;?>
 			  @foreach($elcontrato->singlecontracts as $contract)
-			  <?php $count_nav++;
-				  $date_signed = strtotime($contract->date_signed);
-				  $date_signed = date('d/m/Y',$date_signed);
-			  ?>
+			  <?php $date_signed = strtotime($contract->date_signed);
+				  	$date_signed = date('d/m/Y',$date_signed);?>
 				<li class="active">
 					<a  href="#" data-id="contract-{{$contract->id}}" data-title="{{ $contract->date_signed ? $date_signed : ''}}" class="t_right"></a>
 				</li>
@@ -22,13 +19,11 @@
 	  	<li><a href="#" id="btn-award-nav" class="nav_stage {{ !$elcontrato->singlecontracts ? 'current' : '' }}" data-id="awards" data-title="Adjudicación">
 	  		<?php echo file_get_contents("img/nav_adjudicacion.svg"); ?></a> 
 	  		<ul id="nav_award" {!! $elcontrato->singlecontracts->count() ? "class='hide'" : '' !!}>
-	  		  <?php $count_nav = 0;?>
 	  		  @foreach($elcontrato->awards as $award)
 	  		  <?php 
-	  		      $count_nav++;
 	  		      $time_award = strtotime($award->date);
 	  		      $time_award = date('d/m/Y',$time_award);?>
-	  		  <li><a id="btn-award-<?php echo $count_nav;?>" href="#" data-title="{{ $award->date ? $time_award : ''}}" class="t_right"></a></li>
+	  		  <li><a data-id="award-{{$award->id}}" href="#" data-title="{{ $award->date ? $time_award : ''}}" class="t_right"></a></li>
 	  		  @endforeach
 	  		</ul>
 	  	</li>
