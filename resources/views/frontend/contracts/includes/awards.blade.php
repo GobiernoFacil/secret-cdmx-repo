7ef6e9a06f0106552acbbbb76654bb59f2a95da9
hugovom <hugo.om@gmail.com>
2016-04-04 13:12:36 -0500

    update includes of single contract view

---
+++ resources/views/frontend/contracts/includes/awards.blade.php
@@ -0,0 +1,51 @@
+ <?php foreach($elcontrato->awards as $award):?>
+          <div id="award-<?php echo $award->id;?>">
+            <div class="row divider">
+              <div class="col-sm-12">
+                <p class="title_section">Adjudicación</p>
+                <h1><?php echo $award->title;?></h1>
+                <h2>ID: <?php echo $award->id;?> <span class="label <?php echo $award->status;?>"><?php echo $award->status == "active" ? "ACTIVO":"";?></span></h2>
+              </div>
+            </div>
+            
+            <div class="row divider">
+              <div class="col-sm-6">
+                <p class="title_section">Descripción</p>
+                <p><?php echo $award->description;?></p>
+              </div>
+              <div class="col-sm-6">
+                <h2 class="amount"><span>$</span> <?php echo number_format($award->value,2,'.',',');?> <span><?php echo $award->currency;?></span></h2>
+              </div>
+            </div>
+            
+            <div class="row divider">
+              <div class="col-sm-6">
+                <p class="title_section">Proveedor</p>
+                <p><a href="#"><?php echo $award->suppliers[0]->name;?></a></p>
+              </div>
+              <div class="col-sm-6">
+                <p class="title_section">Fecha</p>
+                <?php $time_award = strtotime($award->date);?>
+                <p><?php echo date('d-m-Y',$time_award);?></p>
+              </div>
+            </div>
+            
+            <div class="row">
+              <div class="col-sm-8">
+                <p class="title_section">ARTÍCULOS</p>
+                <ul>
+                  <?php foreach ($award->items as $item):?>
+                  <li class="row">
+                    <span class="col-sm-9"><?php echo $item->description;?></span>
+                    <span class="col-sm-3"><?php echo $item->quantity;?> <?php echo $item->unit;?></span>
+                  </li>
+                  <?php endforeach;?>             
+                </ul>
+              </div>
+              
+              <div class="col-sm-4">
+                <p class="title_section">DOCUMENTOS</p>
+              </div>
+            </div>
+          </div>
+          <?php endforeach;?>
---
+++ resources/views/frontend/contracts/includes/planning.blade.php
@@ -0,0 +1,26 @@
+        <div id="planning">
+          <div class="row divider">
+            <div class="col-sm-12">
+              <p class="title_section">Planeación</p>
+              <h1><?php echo $elcontrato->planning->budget;?></h1>
+              <h2><?php echo $elcontrato->tender->id;?></h2>
+            </div>
+          </div>
+          <div class="row divider">
+            <div class="col-sm-6">
+              <p class="title_section">PRESUPUESTO (<?php echo $elcontrato->planning->currency;?>)</p>
+              <h2 class="amount"><span>$</span><?php echo number_format($elcontrato->planning->amount,2,'.',',');?></h2>
+            </div>
+            <div class="col-sm-6">
+              <p class="title_section">Fecha</p>
+              <?php $time_planning = strtotime($elcontrato->date);?>
+              <p><?php echo date('d/m/Y',$time_planning);?> </p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-4">
+              <p class="title_section">COMPRADOR</p>
+              <p><a href="/dependencia.php"><?php echo $elcontrato->buyer;?></a></p>
+            </div>
+          </div>
+        </div>
